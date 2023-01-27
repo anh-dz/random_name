3d5d9385277708dc2ed53c8c13a29cb3af74b56c
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2023-01-27 19:13:31 +0700

    Add files via upload

--- control/tmcontrol.py
+++ control/tmcontrol.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from main import *
 
+from . random_name import ran_name
 
 class RAN:
     def __init__(self, widgets: ViewControl):
@@ -11,4 +12,13 @@ class RAN:
         wgs.btn_Press.clicked.connect(self.gift_ran)
     
     def gift_ran(self):
-        pass
+        wgs.btn_Press.setIconSize(QtCore.QSize(64, 64))
+        self.my_qtimer = QTimer()
+        self.my_qtimer.start(100)
+        self.my_qtimer.timeout.connect(lambda: wgs.btn_Press.setIconSize(QtCore.QSize(90, 90)))
+
+        ls = ran_name()
+
+        wgs.btn_Fname.setText(ls[0])
+        wgs.btn_Sname.setText(ls[1])
+
